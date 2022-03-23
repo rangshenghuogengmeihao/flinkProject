a72660ac997570d133321e94423981331f332676
rangshenghuogengmeihao <1744982671@qq.com>
2022-03-23 18:10:25 +0800

    1.0.03

--- src/main/scala3.4/com/shiguang/apitest/sinttest/JdbcSinkTest.scala
+++ src/main/scala3.4/com/shiguang/apitest/sinttest/JdbcSinkTest.scala
@@ -1,12 +1,9 @@
 package com.shiguang.apitest.sinttest
 
-
-import com.mysql.jdbc.PreparedStatement
 import com.shiguang.apitest.{MySensorSource, SensorReading}
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.sink.{RichSinkFunction, SinkFunction}
 import org.apache.flink.streaming.api.scala._
-
 import java.sql
 import java.sql.{Connection, DriverManager}
 
@@ -21,37 +18,37 @@ object JdbcSinkTest {
     val dataStream = inputStream.map(
       data => {
         val arr = data.split(",")
-        SensorReading(arr(0),arr(1).toLong,arr(2).toDouble)
+        SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
       }
     )
 
-    stream.addSink( new MyJdbcSinkFunc )
+    stream.addSink(new MyJdbcSinkFunc)
 
     env.execute("jdbc sink test")
   }
 }
 
-class MyJdbcSinkFunc() extends RichSinkFunction[SensorReading]{
+class MyJdbcSinkFunc() extends RichSinkFunction[SensorReading] {
   //  定义连接、预编译语句
-  var conn:Connection = _
+  var conn: Connection = _
   var insertStmt: sql.PreparedStatement = _
   var updateStmt: sql.PreparedStatement = _
 
   override def open(parameters: Configuration): Unit = {
-    conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root")
+    conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root")
     insertStmt = conn.prepareStatement("insert into sensor_temp (id, temp) values (?,?)")
     updateStmt = conn.prepareStatement("update sensor_temp set temp = ? where id = ?")
   }
 
   override def invoke(value: SensorReading, context: SinkFunction.Context[_]): Unit = {
     //    先执行更新操作，查到就更新
-    updateStmt.setDouble(1,value.temperature)
-    updateStmt.setString(2,value.id)
+    updateStmt.setDouble(1, value.temperature)
+    updateStmt.setString(2, value.id)
     updateStmt.execute()
     //    如果更新没有查到数据，那么就插入
-    if (updateStmt.getUpdateCount == 0){
-      insertStmt.setString(1,value.id)
-      insertStmt.setDouble(2,value.temperature)
+    if (updateStmt.getUpdateCount == 0) {
+      insertStmt.setString(1, value.id)
+      insertStmt.setDouble(2, value.temperature)
       insertStmt.execute()
     }
   }
