50b44092698931579403c3ed64f2b890e9d8f641
rangshenghuogengmeihao <1744982671@qq.com>
2022-03-23 19:57:19 +0800

    1.0.04

--- src/main/scala3.4/com/shiguang/apitest/WindowTest.scala
+++ src/main/scala3.4/com/shiguang/apitest/WindowTest.scala
@@ -1,6 +1,9 @@
 package com.shiguang.apitest
 
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.assigners.{EventTimeSessionWindows, SlidingProcessingTimeWindows, TumblingEventTimeWindows}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow
 
 object WindowTest {
   def main(args: Array[String]): Unit = {
@@ -18,7 +21,12 @@ object WindowTest {
       .map(data => (data.id, data.temperature))
       //.keyBy(data => data._1)  // 按照二元组的第一个元素（id）分组
       .keyBy(_._1)
-//      .window()
+//      .window(TumblingEventTimeWindows.of(Time.seconds(15))) // 滚动时间窗口
+//      .window(SlidingProcessingTimeWindows.of(Time.seconds(15),Time.seconds(3)))  //滑动时间窗口，窗口时间
+//      .window(EventTimeSessionWindows.withGap(Time.seconds(10))) // 会话时间窗口
+//      .timeWindow(Time.seconds(15)) // 时间窗口
+//      .countWindow(10) //滚动窗口
+      .countWindow(10,3) //滑动窗口
 
     env.execute("window test")
   }
