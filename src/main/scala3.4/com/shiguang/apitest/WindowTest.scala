0a50dc697a9fdbcc2b9b07a51a20f117c3aa9df6
rangshenghuogengmeihao <1744982671@qq.com>
2022-03-24 10:09:16 +0800

    1.0.06

--- src/main/scala3.4/com/shiguang/apitest/WindowTest.scala
+++ src/main/scala3.4/com/shiguang/apitest/WindowTest.scala
@@ -1,6 +1,7 @@
 package com.shiguang.apitest
 
 import org.apache.flink.api.common.functions.ReduceFunction
+import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.assigners.{EventTimeSessionWindows, SlidingProcessingTimeWindows, TumblingEventTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
@@ -10,6 +11,7 @@ object WindowTest {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
     // val inputStream = env.readTextFile("src/main/resources/sensor.txt")
     val inputStream = env.socketTextStream("192.168.80.3", 7777)
 
